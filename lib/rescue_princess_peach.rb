65a7edefae71ed632bc6bc0122d529219849f433
Catherine Smiley <catherinesmiley@gmail.com>
2019-12-24 01:57:56 +0000

    Automatically backed up by Learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -7,3 +7,10 @@
 
 # rescue_princess_peach
 # puts status
+def rescue_princess_peach
+  status = "rescued"
+  puts "Hooray! Mario has rescued Princess Peach."
+end
+
+rescue_princess_peach
+puts status
